9dda56ac028051e4065f03b1eaf97d99bcc6cc89
Pascal Ethier <pascal.ethier@dailyxtra.com>
2016-03-16 17:24:08 -0400

    -Added icon for notification -Show notifications on device/browser

--- ChromeWebPushClient/sw.js
+++ ChromeWebPushClient/sw.js
@@ -8,5 +8,31 @@ self.addEventListener('activate', function (event) {
 });
 self.addEventListener('push', function (event) {
     console.log('Push message received', event);
-    // TODO
+    var title = 'Squirt Notification';
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: 'Someone just viewed your profile.',
+      icon: './images/icon.png',
+      tag: 'my-tag',
+	  vibrate:  [300, 100, 400]
+    }));
+});
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('On notification click: ', event.notification.tag);
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url == '/' && 'focus' in client)
+        return client.focus();
+    }
+    if (clients.openWindow)
+      return clients.openWindow('/');
+  }));
 });
