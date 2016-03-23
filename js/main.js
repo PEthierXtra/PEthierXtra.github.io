f09e1ed29ee127355a9b06422f4d1d60769db5d1
Pascal Ethier <pascal.ethier@dailyxtra.com>
2016-03-23 18:02:51 -0400

    Added location to pass back and register the DeviceID

--- js/main.js
+++ js/main.js
@@ -11,9 +11,14 @@ if ('serviceWorker' in navigator) {
             userVisibleOnly: true
         }).then(function (sub) {
             console.log('endpoint:', sub.endpoint);
-			window.prompt("RegistrationID:" , sub.endpoint);
+            //window.prompt("RegistrationID:" , sub.endpoint);
+            //Redirect to the proper place to register the device passing in the
+            //parsed value from the EndPoint
+            var parseArray = sub.endpoint.split("/");
+            window.location = "http://dev.mobile.squirt.org/Profile/SetDeviceId?deviceId=" + parseArray[parseArray.length - 1];
         });
     }).catch(function (error) {
         console.log(':^(', error);
     });
 }
+
