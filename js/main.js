db44db85d992410e551b304c0c07a3cd367febb6
Pascal Ethier <pascal.ethier@dailyxtra.com>
2016-03-23 17:23:10 -0400

    Added error message if pushManager is missing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker is supported');
     navigator.serviceWorker.register('sw.js').then(function (reg) {
         console.log(':^)', reg);
+		if(!('pushManager' in reg)){
+			alert("Sorry...push notification is not supported for your combination of device/browser.");
+			window.history.back();
+			return;
+		}
         reg.pushManager.subscribe({
             userVisibleOnly: true
         }).then(function (sub) {
